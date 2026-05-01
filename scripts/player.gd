5e44eecf2e4425664dd10af4764a7625aa7f933b
Jarmen kell <walczak.adrien.pro@gmail.com>
2026-05-01 16:15:54 +0200

    Add background layer collision and y sort

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const speed = 100
 var current_direction = "none"
+var is_attacking = false
 
 func _ready():
 	$AnimatedSprite2D.play("idle")
@@ -14,58 +15,83 @@ func _physics_process(delta):
 func player_movement(delta):
 	if Input.is_action_pressed("ui_right"):
 		current_direction = "right"
-		play_animation(1)
+		play_movement(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_direction = "left"
-		play_animation(1)
+		play_movement(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
 		current_direction = "down"
-		play_animation(1)
+		play_movement(1)
 		velocity.x = 0
 		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
 		current_direction = "up"
-		play_animation(1)
+		play_movement(1)
 		velocity.x = 0
 		velocity.y = -speed
+	elif Input.is_action_pressed("hit"):
+		play_attack()
+		velocity.x = 0
+		velocity.y = 0
+	elif Input.is_action_pressed("interaction"):
+		play_interaction()
 	else:
-		play_animation(0)
+		play_movement(0)
 		velocity.x = 0
 		velocity.y = 0
 		
 	move_and_slide()
 	
-func play_animation(movement):
-	var direction = current_direction
+func play_movement(movement):
 	var animation = $AnimatedSprite2D
 	
-	if direction == "right":
+	if current_direction == "right":
 		animation.flip_h = false
 		if movement == 1:
 			animation.play("walk_right")
 		elif movement == 0:
 			animation.play("idle_right")
-	if direction == "left":
+	if current_direction == "left":
 		animation.flip_h = true
 		if movement == 1:
 			animation.play("walk_right")
-			#animation.flip_h
 		elif movement == 0:
 			animation.play("idle_right")
 			
-	if direction == "down":
+	if current_direction == "down":
 		animation.flip_h = false
 		if movement == 1:
 			animation.play("walk_down")
 		elif movement == 0:
 			animation.play("idle")
-	if direction == "up":
+	if current_direction == "up":
 		animation.flip_h = false
 		if movement == 1:
 			animation.play("walk_up")
 		elif movement == 0:
 			animation.play("idle_up")
+
+func play_attack():
+	is_attacking = true
+	var animation = $AnimatedSprite2D
+	
+	if current_direction == "right":
+		animation.flip_h = false
+		animation.play("attack_right")
+	if current_direction == "left":
+		animation.flip_h = true
+		animation.play("attack_right")
+	if current_direction == "down":
+		animation.flip_h = false
+		animation.play("attack_down")
+	if current_direction == "up":
+		animation.flip_h = false
+		animation.play("attack_up")
+		
+func play_interaction():
+	var anmation = $AnimatedSprite2D
+	#	TODO
