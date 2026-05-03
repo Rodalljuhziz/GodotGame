f5269edb6b4a3025fa0db29a9752841f8a613bc7
Jarmen kell <walczak.adrien.pro@gmail.com>
2026-05-03 06:44:50 +0200

    Add a combat system with damage, death for enemy and player with timer for animation

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
-var speed : float = 50
+var speed : float = 30
 var player_chase : bool = false
 var player : Node2D = null
+var health : int  = 100
+var player_is_in_attack_zone : bool = false
+var is_slime_alive : bool = true
+
+var can_take_damage : bool = true
 
 func _physics_process(delta : float) -> void:
 	var animation = $AnimatedSprite2D
+	
+	deal_with_damage()
+	
 	if player_chase and player != null:
 		var direction : Vector2 = (player.global_position - global_position).normalized()
 		position += direction * speed * delta
@@ -30,6 +38,34 @@ func _on_detect_player_body_entered(body: Node2D) -> void:
 	player = body
 	player_chase = true
 
-func _on_detect_player_body_exited() -> void:
+func _on_detect_player_body_exited(body : Node2D) -> void:
 	player = null
 	player_chase = false
+
+func enemy() -> void : 
+	pass
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player") :
+		player_is_in_attack_zone = true
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player") :
+		player_is_in_attack_zone = false
+
+func deal_with_damage() -> void :
+	if player_is_in_attack_zone and Global.player_current_hit == true :
+		if can_take_damage == true:
+			health -= 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("slime health : ", health)
+			if health <= 0:
+				health = 0
+				is_slime_alive = false
+				$AnimatedSprite2D.play("death")
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,23 @@ const speed : float = 100
 var current_direction : String = "none"
 var is_attacking : bool = false
 
-func _physics_process(delta : float):
-	player_movement(delta)
+var enemy_is_in_attack_range : bool = false
+var enemy_attack_cooldown : bool = true
+var health : int = 100
+var is_player_alive : bool = true
 
-func player_movement(delta : float):
+var hit_in_progress : bool = false
+var can_take_damage : bool = true
+
+func _physics_process(delta : float) -> void:
+	player_movement(delta)
+	enemy_attack()
+	hit()
+	
+	if health <= 0 : 
+		death()
+
+func player_movement(delta : float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		current_direction = "right"
 		play_movement(1)
@@ -28,10 +41,6 @@ func player_movement(delta : float):
 		play_movement(1)
 		velocity.x = 0
 		velocity.y = -speed
-	elif Input.is_action_pressed("hit"):
-		play_attack()
-		velocity.x = 0
-		velocity.y = 0
 	elif Input.is_action_pressed("interaction"):
 		play_interaction()
 	else:
@@ -41,7 +50,7 @@ func player_movement(delta : float):
 		
 	move_and_slide()
 
-func play_movement(movement : int):
+func play_movement(movement : int) -> void :
 	var animation = $AnimatedSprite2D
 
 	if current_direction == "right":
@@ -49,44 +58,95 @@ func play_movement(movement : int):
 		if movement == 1:
 			animation.play("walk_right")
 		elif movement == 0:
-			animation.play("idle_right")
+			if hit_in_progress == false :
+				animation.play("idle_right")
 	if current_direction == "left":
 		animation.flip_h = true
 		if movement == 1:
 			animation.play("walk_right")
 		elif movement == 0:
-			animation.play("idle_right")
+			if hit_in_progress == false :
+				animation.play("idle_right")
 
 	if current_direction == "down":
 		animation.flip_h = false
 		if movement == 1:
 			animation.play("walk_down")
 		elif movement == 0:
-			animation.play("idle_down")
+			if hit_in_progress == false :
+				animation.play("idle_down")
 	if current_direction == "up":
 		animation.flip_h = false
 		if movement == 1:
 			animation.play("walk_up")
 		elif movement == 0:
-			animation.play("idle_up")
+			if hit_in_progress == false :
+				animation.play("idle_up")
 
-func play_attack():
-	is_attacking = true
-	var animation = $AnimatedSprite2D
-
-	if current_direction == "right":
-		animation.flip_h = false
-		animation.play("attack_right")
-	if current_direction == "left":
-		animation.flip_h = true
-		animation.play("attack_right")
-	if current_direction == "down":
-		animation.flip_h = false
-		animation.play("attack_down")
-	if current_direction == "up":
-		animation.flip_h = false
-		animation.play("attack_up")
-
-func play_interaction():
+#function for interact with e key
+func play_interaction() -> void :
 	var anmation = $AnimatedSprite2D
 	#	TODO
+
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy") :
+		enemy_is_in_attack_range = true
+
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy") : 
+		enemy_is_in_attack_range = false
+
+func enemy_attack() -> void :
+	if enemy_is_in_attack_range and enemy_attack_cooldown == true:
+		if can_take_damage == true:
+			health -= 20 
+			enemy_attack_cooldown = false
+			$attack_cooldown.start()
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("Remaining health : ", health)
+
+func player() -> void:
+	pass
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
+
+func hit() -> void :
+	var direction = current_direction
+	if Input.is_action_just_pressed("hit") :
+		Global.player_current_hit = true
+		hit_in_progress = true
+		if direction == "right" : 
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("attack_right")
+			$hit_duration_animation.start()
+		elif direction == "left" : 
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("attack_right")
+			$hit_duration_animation.start()
+		elif direction == "down" : 
+			$AnimatedSprite2D.play("attack_down")
+			$hit_duration_animation.start()
+		elif direction == "up" : 
+			$AnimatedSprite2D.play("attack_up")
+			$hit_duration_animation.start()
+
+func _on_hit_duration_animation_timeout() -> void:
+	$hit_duration_animation.stop()
+	Global.player_current_hit = false
+	hit_in_progress = false
+	
+func death() -> void:
+	is_player_alive = false #dead player
+	health = 0
+	print("you are dead")
+	$AnimatedSprite2D.play("death")
+	$death_duration_animation.start()
+	self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
